5d448a3dd1bf83fdc3166193b40acdc69f300a73
Mo0000 <Mohossssam@gmail.com>
2025-07-12 18:00:41 +0300

    Commited

---
+++ js/details.module.js
@@ -0,0 +1,38 @@
+import {Ui} from './ui.module.js';
+
+export class Details {
+  constructor(id) {
+    this.ui = new Ui();
+
+    document.getElementById('btnClose').addEventListener('click', () => {
+      document.querySelector('.games').classList.remove('d-none');
+      document.querySelector('.details').classList.add('d-none');
+    });
+
+    this.getDetails(id);
+  }
+
+  getDetails(idGames) {
+    const loading = document.querySelector('.loading');
+    loading.classList.remove('d-none');
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': 'c423aae159msh15accd61a21012cp1ab076jsn49280c20f395',
+        'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
+      },
+    };
+
+    fetch(
+        `'https://free-to-play-games-database.p.rapidapi.com/api/game?id=${
+            idGames}`,
+        options)
+        .then((response) => response.json())
+        .then((response) => this.ui.displayDetails(response))
+        .catch((err) => console.error(err))
+        .finally(() => {
+          loading.classList.add('d-none');
+        });
+  }
+}
